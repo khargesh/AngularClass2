c91326f3544c081ac81320ef4ea30e4860cb2b96
khargesh <61780355+khargesh@users.noreply.github.com>
2020-05-09 21:30:59 +0530

    images and mouse events added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,64 +9,64 @@ import { IProduct } from './IProduct';
 export class AppComponent {
   title = 'T-Shirt-App';
 products:IProduct[]=[];
-
+isInStock=true;
+stockColor='green';
+outofStockColor='red';
 constructor(){
 this.products=this.getProductist();
 }
+
+changeImage(evt,imageUrl)
+{debugger;
+evt.currentTarget.src=imageUrl;
+
+}
+
 
   getProductist():IProduct[]{
 
     return [
 {
   "FronImageUrl":"assets/img/nike_front.jpg",
-  "BacImageUrl":"assets/img/nike_front.jpg",
+  "BacImageUrl":"assets/img/nike_back.jpg",
   "ProductName":"Nike Mens style wear",
-  "Price":120,
+  "Price":110,
   "InStock":80,
   "ProductId":1
 },
 {
-  "FronImageUrl":"assets/img/nike_front.jpg",
-  "BacImageUrl":"assets/img/nike_front.jpg",
-  "ProductName":"Nike Mens style wear",
+  "FronImageUrl":"assets/img/puma_front.jpg",
+  "BacImageUrl":"assets/img/puma_back.jpg",
+  "ProductName":"Puma Mens style wear",
   "Price":120,
-  "InStock":80,
-  "ProductId":1
-
-},
-{
-  "FronImageUrl":"assets/img/nike_front.jpg",
-  "BacImageUrl":"assets/img/nike_front.jpg",
-  "ProductName":"Nike Mens style wear",
-  "Price":120,
-  "InStock":80,
+  "InStock":180,
   "ProductId":1
 
 },
 {
-  "FronImageUrl":"assets/img/nike_front.jpg",
-  "BacImageUrl":"assets/img/nike_front.jpg",
+  "FronImageUrl":"assets/img/levis_front.jpg",
+  "BacImageUrl":"assets/img/levis_back.jpg",
   "ProductName":"Nike Mens style wear",
-  "Price":120,
-  "InStock":80,
+  "Price":130,
+  "InStock":95,
   "ProductId":1
 
 },
 {
-  "FronImageUrl":"assets/img/nike_front.jpg",
-  "BacImageUrl":"assets/img/nike_front.jpg",
-  "ProductName":"Nike Mens style wear",
-  "Price":120,
-  "InStock":80,
+  "FronImageUrl":"assets/img/adidas_front.jpg",
+  "BacImageUrl":"assets/img/adidas_back.jpg",
+  "ProductName":"Adidas Mens style wear",
+  "Price":140,
+  "InStock":120,
   "ProductId":1
 
 },
 {
-  "FronImageUrl":"assets/img/nike_front.jpg",
-  "BacImageUrl":"assets/img/nike_front.jpg",
-  "ProductName":"Nike Mens style wear",
-  "Price":120,
-  "InStock":80,
+  "FronImageUrl":"assets/img/duke_front.jpg",
+  "BacImageUrl":"assets/img/duke_back.jpg",
+  "ProductName":"Duke Mens style wear",
+  "Price":150,
+  "InStock":90,
   "ProductId":1
 
 }
